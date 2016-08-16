ec490fbb9ba7264ef8b7926c236f4dd0669ee8b5
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-15 18:00:14 -0700

    add sign-in

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -10,6 +10,37 @@
 
   var database = firebase.database();
 
+/////////////// AUTHENTICATION ///////////////////////
+
+var provider = new firebase.auth.GoogleAuthProvider();//instance of Google provider 
+
+function signIn(){
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+console.log(result)
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  
+});
+}
+
+
+
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}, function(error) {
+  // An error happened.
+});
 
 
 /////////////// ADD TRAINS TO FIREBASE ON USER INPUT ///////////////////////
@@ -129,3 +160,4 @@ connectionsRef.on("value", function(snap) {//displayed on browser
 	$("#schedulers").html('Number of user working on Master Schedule: ' + snap.numChildren());
 
 });
+
